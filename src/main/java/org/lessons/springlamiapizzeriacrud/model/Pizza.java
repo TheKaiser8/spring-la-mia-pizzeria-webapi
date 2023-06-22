21e0cef820e3110b713af0d78602ed82092b6413
TheKaiser8 <stefano.vanzini8@gmail.com>
2023-06-22 17:11:55 +0200

    Fix Pizza Model (add getters & setters)

--- src/main/java/org/lessons/springlamiapizzeriacrud/model/Pizza.java
+++ src/main/java/org/lessons/springlamiapizzeriacrud/model/Pizza.java
@@ -21,4 +21,44 @@ public class Pizza {
 
     private BigDecimal price;
 
+    public Integer getId() {
+        return id;
+    }
+
+    // In Spring GETTERS & SETTERS sono obbligatori per ogni campo
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public String getPhotoUrl() {
+        return photoUrl;
+    }
+
+    public void setPhotoUrl(String photoUrl) {
+        this.photoUrl = photoUrl;
+    }
+
+    public BigDecimal getPrice() {
+        return price;
+    }
+
+    public void setPrice(BigDecimal price) {
+        this.price = price;
+    }
 }
