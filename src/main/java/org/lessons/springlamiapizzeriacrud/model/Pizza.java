12bf98115b8737508ba4b90d0ce363323f1f1ec4
TheKaiser8 <stefano.vanzini8@gmail.com>
2023-06-26 23:41:30 +0200

    Change UI and fix validation form fields on create page

--- src/main/java/org/lessons/springlamiapizzeriacrud/model/Pizza.java
+++ src/main/java/org/lessons/springlamiapizzeriacrud/model/Pizza.java
@@ -1,10 +1,8 @@
 package org.lessons.springlamiapizzeriacrud.model;
 
 import jakarta.persistence.*;
-import jakarta.validation.constraints.DecimalMin;
-import jakarta.validation.constraints.NotBlank;
-import jakarta.validation.constraints.NotNull;
-import jakarta.validation.constraints.Size;
+import jakarta.validation.constraints.*;
+import org.hibernate.validator.constraints.URL;
 
 import java.math.BigDecimal;
 import java.time.LocalDateTime;
@@ -17,21 +15,24 @@ public class Pizza {
     @GeneratedValue(strategy = GenerationType.IDENTITY) // per indicare l'AUTO-INCREMENT
     private Integer id;
 
-    @NotBlank(message = "Il titolo è obbligatorio, il campo non può essere vuoto")
-    @Size(min = 1, max = 100, message = "Il titolo deve avere un numero di caratteri compreso tra 1 e 100")
+    @NotBlank(message = "Il nome è obbligatorio, il campo non può essere vuoto")
+    @Size(min = 1, max = 100, message = "Il nome deve avere un numero di caratteri compreso tra 1 e 100")
     @Column(nullable = false)
     private String name;
 
     @NotBlank(message = "La descrizione è obbligatoria, il campo non può essere vuoto")
-    @Size(min = 1, max = 250, message = "La descrizione deve avere un numero di caratteri compreso tra 1 e 250")
+    @Size(min = 1, max = 500, message = "La descrizione deve avere un numero di caratteri compreso tra 1 e 500")
+    @Column(columnDefinition = "TEXT")
     private String description;
 
     @NotBlank(message = "URL immagine è obbligatorio, il campo non può essere vuoto")
-    @Size(min = 1, max = 250, message = "URL deve avere un numero di caratteri compreso tra 1 e 250")
+    @Size(min = 1, max = 500, message = "URL deve avere un numero massimo di 500 caratteri")
+    @URL(message = "Il campo URL deve essere un indirizzo valido")
     private String photoUrl;
 
     @NotNull(message = "Il prezzo è obbligatorio, non può essere nullo")
-    @DecimalMin(value = "0.01", message = "Il prezzo deve essere maggiore o uguale a zero")
+    @DecimalMin(value = "0.00", message = "Il prezzo deve essere maggiore o uguale a zero")
+    @DecimalMax(value = "99.99", message = "Il prezzo deve essere minore o uguale a 99.99")
     private BigDecimal price;
 
     private LocalDateTime createdAt;
