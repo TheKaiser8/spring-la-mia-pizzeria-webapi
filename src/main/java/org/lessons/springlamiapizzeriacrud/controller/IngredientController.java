131f3d24d15d3966f646e0bd89e0690782057f41
TheKaiser8 <stefano.vanzini8@gmail.com>
2023-06-30 18:04:59 +0200

    Add editing & deleting of an ingredient on index page - add delete method in IngredientController and implement RequestParam to edit in index method

--- src/main/java/org/lessons/springlamiapizzeriacrud/controller/IngredientController.java
+++ src/main/java/org/lessons/springlamiapizzeriacrud/controller/IngredientController.java
@@ -7,12 +7,10 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.validation.BindingResult;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.ModelAttribute;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
+import java.util.Optional;
 
 @Controller
 @RequestMapping("/ingredients")
@@ -22,14 +20,31 @@ public class IngredientController {
     private IngredientRepository ingredientRepository;
 
     // metodo index gestisce sia la lista degli ingredienti presenti sul database che il form per creare o modificare un ingrediente
+    // passo un parametro opzionale con l'id dell'ingrediente che voglio modificare
     @GetMapping
-    public String index(Model model) {
+    public String index(Model model, @RequestParam("editIngredient") Optional<Integer> ingredientId) {
         // recupero da database tutti gli ingredienti
         List<Ingredient> ingredientList = ingredientRepository.findAll(); // findAll restituisce una lista vuota se non trova elementi, non restituisce null
         // passo al model un attributo ingredients con tutti gli ingredienti
         model.addAttribute("ingredients", ingredientList);
+        // definisco un oggetto Ingredient
+        Ingredient ingredientObj;
+        // se ho il parametro ingredientId cerco l'ingrediente corrispondente su database
+        if (ingredientId.isPresent()) {
+            Optional<Ingredient> ingredientDb = ingredientRepository.findById(ingredientId.get());
+            // se è presente l'oggetto su db, ingredientObj diventa l'oggetto ingredientDb
+            if (ingredientDb.isPresent()) {
+                ingredientObj = ingredientDb.get();
+            } else {
+                // se NON è presente l'oggetto su db, ingredientObj diventa un nuovo oggetto vuoto
+                ingredientObj = new Ingredient();
+            }
+        } else {
+            // se NON ho il parametro opzionale, ingredientObj diventa un nuovo oggetto vuoto
+            ingredientObj = new Ingredient();
+        }
         // passo al model un attributo ingredientObj per mappare il form su un oggetto di tipo Ingredient
-        model.addAttribute("ingredientObj", new Ingredient());
+        model.addAttribute("ingredientObj", ingredientObj);
         return "/ingredients/index";
     }
 
@@ -45,6 +60,12 @@ public class IngredientController {
         // dopo la validazione salvo il formIngredient
         ingredientRepository.save(formIngredient);
         // fa la redirect alla index
+        return "redirect:/ingredients";
+    }
+
+    @PostMapping("/delete/{id}")
+    public String delete(@PathVariable Integer id) {
+        ingredientRepository.deleteById(id);
         return "redirect:/ingredients";
     }
 }
