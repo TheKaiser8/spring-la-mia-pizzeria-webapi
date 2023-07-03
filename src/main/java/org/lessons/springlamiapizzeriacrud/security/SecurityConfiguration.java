a9014ef0b3ca3e19c9ae86ebae2956d71f78b138
TheKaiser8 <stefano.vanzini8@gmail.com>
2023-07-03 17:59:26 +0200

    Refactoring route SecurityConfiguration

--- src/main/java/org/lessons/springlamiapizzeriacrud/security/SecurityConfiguration.java
+++ src/main/java/org/lessons/springlamiapizzeriacrud/security/SecurityConfiguration.java
@@ -2,6 +2,7 @@ package org.lessons.springlamiapizzeriacrud.security;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpMethod;
 import org.springframework.security.authentication.dao.DaoAuthenticationProvider;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.crypto.factory.PasswordEncoderFactories;
@@ -38,17 +39,29 @@ public class SecurityConfiguration {
         return provider;
     }
 
+    /*
+        ROTTE da proteggere:
+        /pizzas/create ADMIN
+        /pizzas/** ADMIN e USER
+        /pizzas ADMIN e USER
+        /pizzas/edit/**
+        /pizzas/delete/** ADMIN
+        /offers/** ADMIN
+        /ingredients/** ADMIN
+     */
+
     @Bean
     SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
         // definisco la catena di filtri
         http.authorizeHttpRequests()
                 .requestMatchers("/pizzas/create").hasAuthority("ADMIN")
                 .requestMatchers("/pizzas").hasAnyAuthority("ADMIN", "USER")
-                .requestMatchers("/pizzas/{id}").hasAnyAuthority("ADMIN", "USER")
-                .requestMatchers("/pizzas/edit/{id}").hasAuthority("ADMIN")
-                .requestMatchers("/pizzas/delete/{id}").hasAuthority("ADMIN")
+                .requestMatchers("/pizzas/**").hasAnyAuthority("ADMIN", "USER")
+                .requestMatchers("/pizzas/edit/**").hasAuthority("ADMIN")
+                .requestMatchers("/pizzas/delete/**").hasAuthority("ADMIN")
                 .requestMatchers("/offers/**").hasAuthority("ADMIN")
                 .requestMatchers("/ingredients/**").hasAuthority("ADMIN")
+                .requestMatchers(HttpMethod.POST).hasAuthority("ADMIN")
                 .requestMatchers("/**").permitAll()
                 .and().formLogin()
                 .and().logout();
